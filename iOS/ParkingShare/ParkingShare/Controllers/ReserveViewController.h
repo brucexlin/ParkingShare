cc3bd35f0c5fce9e3b201838073a3c8025aa6f89
Bruce Lin <xinda.lin@oracle.com>
2015-04-25 01:37:27 -0700

    reserve

---
+++ iOS/ParkingShare/ParkingShare/Controllers/ReserveViewController.h
@@ -0,0 +1,13 @@
+//
+//  ReserveViewController.h
+//  ParkingShare
+//
+//  Created by Xinda Lin on 4/25/15.
+//  Copyright (c) 2015 Qing Sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReserveViewController : UIViewController
+
+@end
---
+++ iOS/ParkingShare/ParkingShare/Controllers/ReserveViewController.m
@@ -0,0 +1,37 @@
+//
+//  ReserveViewController.m
+//  ParkingShare
+//
+//  Created by Xinda Lin on 4/25/15.
+//  Copyright (c) 2015 Qing Sun. All rights reserved.
+//
+
+#import "ReserveViewController.h"
+
+@interface ReserveViewController ()
+
+@end
+
+@implementation ReserveViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
